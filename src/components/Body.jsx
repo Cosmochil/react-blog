66e413df16380b58439d54f03e0b91fc288e2348
Cosmo <76464517+Cosmochil@users.noreply.github.com>
2021-08-07 14:04:23 +0700

    Add files via upload

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,33 +1,44 @@
 import { React, useState, useEffect } from "react";
 import styled from "styled-components";
-import BlogLists from './BlogLists'
+import BlogLists from "./BlogLists";
 
 function Body() {
   const [articles, setArticles] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const Body = styled.div``;
 
   const deleteBlog = (id) => {
     const newArticles = articles.filter((article) => article.id !== id);
     setArticles(newArticles);
-  }
+  };
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setArticles(data)
-        setIsLoading(false)
-      })
-  }, [])
-  
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Failed to fetch");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setArticles(data);
+          setIsLoading(false);
+          setError(null)
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 500);
+  }, []);
 
   return (
     <Body>
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
-      {articles && <BlogLists blogs={articles} deleteBlog={ deleteBlog }/>}
+      {articles && <BlogLists blogs={articles} deleteBlog={deleteBlog} />}
     </Body>
   );
 }
